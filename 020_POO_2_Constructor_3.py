bf99139f0afe28185e0999041b2fa00ea905a4ad
Iban99 <miriamiban@hotmail.com>
2024-05-18 18:35:26 +0200

    poo

--- 020_POO_2_Constructor_3.py
+++ 020_POO_2_Constructor_3.py
@@ -16,4 +16,20 @@ print(persona1.nombre)
 print(persona1.apellido)
 print(persona1.edad)
 
+class Libro:
+    def __init__(self, titulo, autor, anio_publicacion):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicacion = anio_publicacion
+    
+    def descricpion(self):
+        return f"{self.titulo}por {self.autor}"
+    
+    def is_antiguo(self):
+        return self.anio_publicacion < 1910
 
+libro1 = Libro('1', 'Juan', 1967)
+libro2 = Libro('2', 'Jose', 1605)
+
+libro1.is_antiguo()
+libro2.is_antiguo()
